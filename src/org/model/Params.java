c9e95eefda3bd5389addf46cf8b791823c341ecc
tndoan <doan.thanh.nam.1988@gmail.com>
2015-11-13 14:23:05 +0800

    change params

--- src/org/model/Params.java
+++ src/org/model/Params.java
@@ -2,10 +2,10 @@ package org.model;
 
 public class Params {
 	public static final double alpha = 0.4; // parameter for backtracking
-	public static final double beta = 0.9; // parameter for backtracking
+	public static final double beta = 0.7; // parameter for backtracking
 	
 	/**
 	 * threshold to terminate
 	 */
-	public static final double threshold = 0.001;
+	public static final double threshold = 0.01;
 }
