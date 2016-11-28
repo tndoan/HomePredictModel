d11e9e4f4234882e1810f956fa59db5641fe6561
tndoan <doan.thanh.nam.1988@gmail.com>
2016-11-28 21:50:36 +0800

    add class to store the mode to indicate model

---
+++ src/org/model/ModeModel.java
@@ -0,0 +1,7 @@
+package org.model;
+
+public class ModeModel {
+	public static int DISTANCE_AREAATTRACTION = 1;
+	public static int NEIGHBORHOOD_COMPETITION = 2;
+	public static int COMBINED = 3;
+}
